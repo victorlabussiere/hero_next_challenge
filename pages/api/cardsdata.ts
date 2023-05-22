148e0ab833dc83b89102a73ea50c085f734a0c37
Victor Labussiere <victor.labussiere95@gmail.com>
2023-05-22 00:04:04 -0300

    mock data for deploy

--- pages/api/cardsdata.ts
+++ pages/api/cardsdata.ts
@@ -3,9 +3,30 @@ import { CardData } from '../../components/ui/Cards/types'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<CardData[]>) {
 
+  const mockData: CardData[] = [
+    {
+      imgPath: "icon-trilhas",
+      imgAlt: "icon-trilhas",
+      cardTitle: "Trilhas e Etapas",
+      cardText: "Crie planos de estudos especificando aulas e/ou cursos e definindo a ordem que seus alunos devem estudar."
+    },
+    {
+      imgPath: "icon-playlists",
+      imgAlt: "icon-playlists",
+      cardTitle: "Playlists",
+      cardText: "Transforme uma coleção em uma playlist para poder ver vídeos e áudios em sequência offline."
+    },
+    {
+      imgPath: "icon-folder",
+      imgAlt: "icon-playlists",
+      cardTitle: "Coleções",
+      cardText: "Crie coleções, adicione conteúdos, reorganize ítens e deixe tudo do seu jeito para melhorar a experiência."
+    }
+  ]
+
   const data: Promise<CardData[]> = fetch('http://localhost:3001/cards')
     .then(res => res.json())
     .catch(err => (err.message).json())
 
-  return await res.status(200).json(await data)
+  return res.status(200).json(await data || mockData)
 }
